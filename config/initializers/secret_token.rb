c28dbba9e5fc7f45681a485eceb4cd96e7953132
Alexander Chubarkin <a.chubarkin@gmail.com>
2013-05-06 22:00:49 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MySql::Application.config.secret_token = '63b683ccb256cd14baee417d9e2bb44ca57cf8c44bf775eca46132b9231200dc72c35f7a8cc9a9e51b4fa4f144fdb6a1eb5c00382d187a55e1a3fb61a523034d'
